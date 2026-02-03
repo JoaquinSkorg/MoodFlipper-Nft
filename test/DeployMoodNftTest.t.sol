baf3cd22bcfbb511f95d858490f3338afc5546fc
skorggg <adesekemiayo@gmail.com>
2026-02-03 15:10:17 +0100

    MoodFlipperNft

---
+++ test/DeployMoodNftTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+import {Test} from "forge-std/Test.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToUri() public view {
+        // SVG with NO trailing newline
+        string
+            memory svg = '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">'
+            '<circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />'
+            "</svg>";
+
+        string memory actualUri = deployer.svgToImageURI(svg);
+
+        // Build expected URI exactly how the contract should
+        string memory expectedUri = string(
+            abi.encodePacked(
+                "data:image/svg+xml;base64,",
+                Base64.encode(bytes(svg))
+            )
+        );
+
+        assertEq(actualUri, expectedUri);
+    }
+}
---
+++ test/integrations/BasicNftTest.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "../../src/BasicNft.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PAL =
+        "ipfs://bafybeihdwdcefgh4dqkjv67uzcmw7ojee6xedzdetojuzjevtenxquvyku/";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testThatNameIsCorrect() public view {
+        string memory expectedName = "DogPals";
+        string memory actualName = basicNft.name();
+
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PAL);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(basicNft.tokenURI(0))) ==
+                keccak256(abi.encodePacked(PAL))
+        );
+    }
+}
